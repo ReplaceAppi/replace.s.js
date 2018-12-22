b680262cf4d98d25e3f72214a4d280325e884fb8
Quales <arthurleplusdur.al@gmail.com>
2018-12-22 17:36:18 +0100

    Create config.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const expressJs = require("express");
+const express = require("express");
 const mysql = require("sync-mysql");
 const uuid = require("uuid/v1");
 
 const Collection = require("./struct/Collection");
+const config = require("./config");
+
+const app = express();
+app.listen(confit.port, () => {
+	console.log(`Server running on port ${config.port}`);
+})
